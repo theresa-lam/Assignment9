bff4748eba5a2763fb916fa876b99cb0a0ab1962
theresa-lam <47282780+theresa-lam@users.noreply.github.com>
2019-03-31 17:21:07 -0600

    Update Bank.java

--- Bank.java
+++ Bank.java
@@ -1,9 +1,40 @@
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.PriorityQueue;
+import java.util.TreeSet;
 
-public class Bank{
+public class Bank {
+	PriorityQueue<Customer> queuedCustomers = new PriorityQueue<Customer>();
+	private TreeSet<Customer> customers = new TreeSet<Customer>();
+	private HashSet<BankAccount> accounts = new HashSet<BankAccount>();
+	
+	public void queueCustomer(Customer a) {
+		queuedCustomers.add(a);
+	}
+	
+	public  Customer nextCustomer() {
+		
+			return queuedCustomers.poll();
+	
+	}
+	
+	
 
-    private HashSet<BankAccount> accounts = new HashSet<BankAccount>();
+
+	public void addCustomer (Customer customer)
+	{
+		this.customers.add(customer);
+	}
+
+	public Customer [] getCustomers()
+	{
+		return customers.toArray(new Customer [customers.size()]);
+	}
+
+
+	
+
+	
 
     public void addAccount(BankAccount account) throws DuplicateAccountException {
         if (accounts.add(account)){
@@ -13,15 +44,21 @@ public class Bank{
             throw new DuplicateAccountException("This account already exists and has not been added");
         }
     }
+	
 
-    public BankAccount[] getAccounts() {
-        Iterator iterator = accounts.iterator();
-        BankAccount[] anArray = new BankAccount[accounts.size()];
-        while (iterator.hasNext()) {
-            for (int i=0; i<=accounts.size(); i++){
-                anArray[i] = (BankAccount)iterator.next();
-            }
-        }
-        return anArray;
-    }
+		 public BankAccount[] getAccounts() {
+		        Iterator iterator = accounts.iterator();
+		        BankAccount[] anArray = new BankAccount[accounts.size()];
+		        while (iterator.hasNext()) {
+		            for (int i=0; i<=accounts.size(); i++){
+		                anArray[i] = (BankAccount)iterator.next();
+		            }
+		        }
+		        return anArray;
+		    }
+
+
+	
+	
 }
+
