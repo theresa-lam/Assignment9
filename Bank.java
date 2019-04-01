e101cb38257ef438a466c75810440abc6628f57f
haydenkerr1 <47900367+haydenkerr1@users.noreply.github.com>
2019-04-01 16:55:49 -0600

    2 ERRORS REMAINING, FIXED BRACKETING

--- Bank.java
+++ Bank.java
@@ -2,60 +2,46 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.PriorityQueue;
 import java.util.TreeSet;
+import java.util.Arrays;
+import java.util.ArrayList;
 
 public class Bank  {
-	PriorityQueue<Customer> queuedCustomers = new PriorityQueue<Customer>();
-	private TreeSet<Customer> customers = new TreeSet<Customer>();
-	private HashSet<BankAccount> accounts = new HashSet<BankAccount>();
-	private Iterator iterator = accounts.iterator();
-	private BankAccount[] anArray = new BankAccount[accounts.size()];
-	
-	public void queueCustomer(Customer a) {
-		queuedCustomers.add(a);
-	}
-	
-	public  Customer nextCustomer() {
-		
-			return queuedCustomers.poll();
-	
-	}
-	
-	
+    PriorityQueue<Customer> queuedCustomers = new PriorityQueue<Customer>();
+    private TreeSet<Customer> customers = new TreeSet<Customer>();
+    private HashSet<BankAccount> accounts = new HashSet<BankAccount>();
+    private Iterator iterator = accounts.iterator();
+    private BankAccount[] anArray = new BankAccount[accounts.size()];
 
+    public void queueCustomer(Customer a) {
+        queuedCustomers.add(a);
+    }
 
-	public void addCustomer (Customer customer)
-	{
-		this.customers.add(customer);
-	}
+    public  Customer nextCustomer() {
+        return queuedCustomers.poll();
+    }
 
-	public Customer [] getCustomers()
-	{
-		return customers.toArray(new Customer [customers.size()]);
-	}
+    public void addCustomer (Customer customer) {
+        this.customers.add(customer);
+    }
+
+    public Customer [] getCustomers() {
+        return customers.toArray(new Customer [customers.size()]);
+    }
 
 
     public void addAccount(BankAccount account) throws DuplicateAccountException {
-        if (accounts.add(account)){
+        if (accounts.contains(account) == false){
             accounts.add(account);
         }
         else{
             throw new DuplicateAccountException("This account already exists and has not been added");
         }
     }
-	
+
 
     public BankAccount[] getAccounts() {
-		   
-		   while (iterator.hasNext()) {
-		       for (int i=0; i<=accounts.size(); i++){
-		            anArray[i] = (BankAccount)iterator.next();
-		            }
-		        }
-		       return anArray;
-		    }
-
-
-	
-	
+        ArrayList<BankAccount> list = new ArrayList<BankAccount>(accounts);
+        return list.toArray(anArray);
+    }
 }
 
